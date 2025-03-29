2427d30f1f4eaa90d878b4ff81cd84e3ed319493
Brayden Zhang <bandb252525@gmail.com>
2025-03-28 23:05:46 -0400

    update

--- sites.js
+++ sites.js
@@ -10,6 +10,25 @@ const sites = [
             twitter: "https://twitter.com/brayden__zhang"
         }
     },
-    
+    {
+        name: "Eric Li",
+        year: "2027",
+        program: "Computer Science",
+        url: "https://www.linkedin.com/in/eric-li-287869286/", 
+        social: {
+            linkedin: "https://www.linkedin.com/in/eric-li-287869286/",
+            github: "https://github.com/lieri123",
+        }
+    },
+  
+    {
+        name: "Aditya Pisharoty",
+        year: "2027",
+        program: "Computer Engineering",
+        url: "https://www.linkedin.com/in/aditya-pisharoty/", 
+        social: {
+            linkedin: "https://www.linkedin.com/in/aditya-pisharoty/",
+        }
+    },
   
 ];
